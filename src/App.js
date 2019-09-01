d655c188efb110b3d1a8cc8de7ed8b4f7ca512a5
Andrew Leong <andrewleong1993@gmail.com>
2019-09-01 23:36:01 +0800

    Add counter and posts for useState example

--- src/App.js
+++ src/App.js
@@ -1,21 +1,48 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Fragment, useState } from 'react';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+const App = () => {
+  const initialCount = 0
+  const [count, setCount] = useState(initialCount)
+  const [posts, setPosts] = useState([
+    {
+      title: 'Andrew is cool',
+      body: 'Some text here'
+    }
+  ])
+
+  return (
+    <Fragment>
+
+      <h3>Count: {count}</h3>
+      <button onClick={() => setCount(prevCount => prevCount + 1)}>Plus +</button>
+      <button onClick={() => setCount(prevCount => prevCount - 1)}>Minus -</button>
+      <button onClick={() => setCount(initialCount)}>Reset</button>
+
+      {
+        posts.map((post, key) => {
+          return (
+            <div key={key}>
+              <hr />
+              <h3>Posts: </h3>
+              <p>Title: {post.title}</p>
+              <p>Body: {post.body}</p>
+            </div>
+          )
+        })
+      }
+      <button onClick={() => {
+        setPosts(
+          [
+            ...posts,
+            {
+              title: 'Batman is cool',
+              body: 'Another text here'
+            }
+          ]
+        )
+      }}>Add Post</button>
+    </Fragment>
+  )
 }
 
 export default App;
